336b3f7e4094cb13b9839221e3ae15aa294427fd
Thanushree L <138572941+thanushree024@users.noreply.github.com>
2025-01-08 14:16:54 +0530

    Coverage_Analysis.R

---
+++ Task_1/Sub_Task_1/Coverage_Analysis.R
@@ -0,0 +1,22 @@
+# Load necessary libraries
+library(dplyr)
+library(ggplot2)
+setwd("C:/Users/Thanu/Downloads")
+# Load data
+data <- read.csv("PupilBioTest_PMP_revA_task1.csv")
+# Calculate coverage (number of occurrences of each CpG site for each tissue)
+coverage_data <- data %>%
+  group_by(Tissue, CpG_Coordinates) %>%
+  summarise(Coverage = n())
+
+# Calculate median and CV for each tissue
+coverage_stats <- coverage_data %>%
+  group_by(Tissue) %>%
+  summarise(
+    Median = median(Coverage),
+    CV = (sd(Coverage) / mean(Coverage)) * 100
+  )
+
+# Print the results
+print(coverage_stats)
+
